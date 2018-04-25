32eac23fe36ab2fb72e315ed8138ec6655914f17
Olga D <o.dmitricenko@gmail.com>
2018-04-25 15:50:20 +0200

    all plyrconfig and consts

---
+++ src/misc/plyr-config.js
@@ -0,0 +1,29 @@
+export const PLYR_CONFIG = {
+    autoplay: false,
+    controls: [], //pass empty array to disable plyr controls
+    posterImage: true,
+    enabled: true,
+    loadSprite: true,
+    iconPrefix: 'plyr',
+    debug: true,
+    seekTime: 10,
+    volume: 5,
+    clickToPlay: false  ,
+    disableContextMenu: true,
+    hideControls: true,
+    showPosterOnEnd: false,
+    keyboardShortcuts: {
+      focused: true,
+      global: false
+    },
+    tooltips: {
+      controls: false,
+      seek: true
+    },
+    displayDuration: true,
+    storage: {
+      enabled: true,
+      key: 'plyr_volume'
+    }
+  }
+  
---
+++ src/misc/states.js
@@ -0,0 +1,8 @@
+export const LOADING = 'LOADING' //aka BUFFERING
+export const READY = 'READY' //need for isReady
+export const PLAYING = 'PLAYING'
+export const PAUSED = 'PAUSED'
+export const STOPPED = 'STOPPED'
+
+export const MUTED = 'MUTED'
+export const FULLSCREEN = 'FULLSCREEN'
